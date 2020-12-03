b0addf9f343b3769af0d8c2328a5de57624b3f9f
Interactics <56077549+Interactics@users.noreply.github.com>
2020-12-03 14:04:24 +0900

    added rpm2vel and vel2rpm

--- motorctrldesign/motorctrldesign.py
+++ motorctrldesign/motorctrldesign.py
@@ -6,4 +6,18 @@ def PulsePerHZ(r, x, v, ppr, f) :
     # 목표 속도 v[m/s], 바퀴반지름 r[m], 축 한바퀴당 펄스 수 ppr, 체배 x, 제어주기 f
  
     return v*1/(2*3.141592*r) * ppr * x / f
-    
+
+
+def vel2rpm(v, r) :
+    # r은 바퀴 반지름 [m]
+    # velocity -> RPM
+    # Velocity [m/s] -> RPM
+    # v 속도는 몇 rpm일까?
+
+    return v / (2 * 3.141592 * r) * 60 
+
+def rpm2vel(rpm, r) : 
+    # R은 m
+    # RPM -> vel [m/s]
+    # rpm은 몇 vel일까?
+    return rpm * 2 * PI * r / 60 
