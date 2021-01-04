7f5f4882aa2631909a5f0386a8a12bb5f8e803f5
Interactics <56077549+Interactics@users.noreply.github.com>
2021-01-04 12:38:58 +0900

    Word correction
    Control interval -> Control frequency

--- motorctrldesign/motorctrldesign.py
+++ motorctrldesign/motorctrldesign.py
@@ -3,7 +3,7 @@ def PulsePerHZ(r, x, v, ppr, f) :
     # Pulse Per HZ
     # Velcity[m/s] -> Pulse per HZ 
     # 제어주기 당 펄스가 얼마나 나오는지 확인할 때 사용하는 함수
-    # 목표 속도 v[m/s], 바퀴반지름 r[m], 축 한바퀴당 펄스 수 ppr, 체배 x, 제어주기 f
+    # 목표 속도 v[m/s], 바퀴반지름 r[m], 축 한바퀴당 펄스 수 ppr, 체배 x, 제어주파수 f [
  
     return v*1/(2*3.141592*r) * ppr * x / f
 
